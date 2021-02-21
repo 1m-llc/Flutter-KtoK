b4e58b1596315b5eae07e893d73095f97c494b59
imoto <hideyuki.imoto@noin.tv>
2021-02-22 00:00:55 +0900

    fluroバージョンアップ対応

--- lib/presentation/views/app_component.dart
+++ lib/presentation/views/app_component.dart
@@ -1,16 +1,15 @@
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ktok/domain/providers/app_theme_data_notifier.dart';
-import 'package:provider/provider.dart';
-import 'package:fluro/fluro.dart';
-import 'package:flutter_ktok/presentation/navigations/routes.dart';
-import 'package:flutter_ktok/presentation/constants/themes/theme_data_light.dart';
 import 'package:flutter_ktok/presentation/constants/themes/theme_data_dark.dart';
+import 'package:flutter_ktok/presentation/constants/themes/theme_data_light.dart';
+import 'package:flutter_ktok/presentation/navigations/routes.dart';
+import 'package:provider/provider.dart';
 
 class AppComponent extends StatelessWidget {
-
   AppComponent() {
     // ルーターの初期化
-    final router = Router();
+    final router = FluroRouter();
     Routes.configureRoutes(router);
     Application.router = router;
   }
@@ -26,5 +25,3 @@ class AppComponent extends StatelessWidget {
     return app;
   }
 }
-
-
